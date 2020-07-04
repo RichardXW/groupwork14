9be9d1c66a83cb42b413bb1afd6ae8f88b16ba6d
Zayn <58586624+Zayn-Tang@users.noreply.github.com>
2020-07-05 02:51:31 +0800

    Update get_dev_infor.py

--- final_groupwork/get_dev_infor.py
+++ final_groupwork/get_dev_infor.py
@@ -5,17 +5,18 @@ import subprocess
 
 __author__ = "Zunye Tang, Xiangwen Qiao, Wenyao Chen, Bofei Zhang Data Scicence, LZU University "
 __copyright__ = "Copyright © 2020, Group 14, Zunye Tang"
-__version__ = 0.1
+__version__ = 0.2
 
 
-class Full_infor:
+class Infor:
     def __init__(self,version='v4.4'):
         self.version = version
         self.repo = "C:\\Users\\17301\\Desktop\\linux-stable\\"
-        self.csv = "C:\\Users\\17301\\Desktop\\数据科学编程\\name.csv"
+        self.dev = "C:\\Users\\17301\\Desktop\\数据科学编程\\out.csv"
         self.name_set = set()
 
-    def infor(self,set_name):
+
+    def infor_(self,set_name):
         ls = []
         cmd1 = 'git log --pretty=format:"%an,%cd,% ae" --author="'
         cmd2 = '" -1 --date=short'
@@ -25,7 +26,7 @@ class Full_infor:
             get_name_infor = get_name_infor.communicate()[0].decode("utf-8").split(",")
             ls.append(get_name_infor)
             df = pd.DataFrame(ls,columns=["name", "commit_time", "email"])
-        print(df)
+        return df
 
 
     def get_developer_name(self,cmd):
@@ -37,15 +38,32 @@ class Full_infor:
         return subprocess.Popen(cmd, cwd=self.repo, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,
                          shell=True)
 
+    def get_time(self,dataframe):
+        for i in self.name_set:
+            cmd = 'git log --pretty=format:"%cd" --author="%s"' % (
+                i) + " -1 --date=short"
+            p = subprocess.Popen(cmd, cwd=self.repo, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
+            last = p.communicate()[0]
+            cmd2 = 'git log -1 --pretty=format:"%cd" --author="%s"' % (
+                i) + " --reverse --date=short"
+            p2 = subprocess.Popen(cmd, cwd=self.repo, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
+            start = p.communicate()[0]
+            dataframe = pd.concat([dataframe,last,start],axis=1,ignore_index=True)
+        return dataframe
+
+
     def run(self):
         rev1 = self.version
-        cmd = " git log --pretty=format:'%cn' v4.4 --follow kernel/sched/core.c "
+        cmd = " git log --pretty=format:'%cn' "+rev1+" --follow kernel/sched/core.c "
         git_name_list = self.get_developer_name(cmd)
         raw_counts = git_name_list.communicate()[0]
         for name in raw_counts.decode("utf-8").split("\n"):
             name = name.strip("'")
             self.name_set.add(name)
-        self.infor(self.name_set)
+        df = self.infor_(self.name_set)
+        df2 = self.get_time(df)
+        return df2
 
 
-infor = Full_infor().run()
+infor = Infor().run()
+print(infor)
