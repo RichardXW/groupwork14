68f19c3ca96bfaac0c9a683e41e0fa457f5aa945
Zayn <58586624+Zayn-Tang@users.noreply.github.com>
2020-07-05 10:56:40 +0800

    Update and rename cluster_dev_level to cluster_dev_level.py

--- final_groupwork/cluster_dev_level.py
+++ final_groupwork/cluster_dev_level.py
@@ -7,12 +7,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 
-
-__author__ = "Zunye Tang, Xiyuan Chang, Xiangwen Qiao, Wenyao Chen, Bofei Zhang Data Scicence, LZU University "
-__copyright__ = "Copyright © 2020, Group 14"
-__version__ = 0.1
-
-
 '''
 regressor = DecisionTreeRegressor(criterion='mse', max_depth=40,
                             max_features=None, max_leaf_nodes=None, min_samples_leaf=1,
@@ -20,27 +14,26 @@ regressor = DecisionTreeRegressor(criterion='mse', max_depth=40,
                             presort=False, random_state=None, splitter='best')
 '''
 
-developer = pd.read_csv("out.csv",encoding="iso8859-1")
+developer = pd.read_csv("../out.csv",encoding="iso8859-1")
 df = pd.DataFrame(developer)
-score = df[["Score"]]
 factors = [
     "duration(day)",
     "Commit_times"
 ]
-
+score = df[["Score"]].apply(lambda x:int(x/10) , axis=1)
 
 X_train,X_test,y_train,y_test = train_test_split(df[factors],score,test_size=0.3,random_state=0)
 
 '''
 Try with decisionTreeClassifier
 '''
-tmp = y_test.loc[:,"Score"]
+
 X_ax = np.arange(len(y_test))
 classifier = DecisionTreeClassifier(max_depth=20)
 clf = classifier.fit(X_train,y_train)
 predict_X = classifier.predict(X_test)
 score= classifier.score(X_test,y_test)
-print(score) 
+print(score)
 plt.scatter(X_ax,predict_X,c="red",label="predict_X")
 plt.scatter(X_ax,y_test,c="blue",label="y_test")
 plt.legend()
